291de283eb7115ef7fe10131364f57e836afb805
mrFlick72 <valerio.vaudi@gmail.com>
2019-08-12 23:42:15 +0200

    front end in account service

---
+++ src/main/frontend/webpack.config.js
@@ -0,0 +1,44 @@
+var path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const BUID_DIR = path.resolve(__dirname + "../../../../target/classes/static");
+
+module.exports = {
+    mode: 'production',
+    entry: {
+        account: path.resolve(__dirname, './app/index.js'),
+    },
+    resolve: {
+        extensions: [".js", ".jsx"]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            chunks: ['account'],
+            filename: "index.html",
+            template: path.resolve(__dirname, "../resources/static/index.html")
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: path.join(__dirname, "."),
+                exclude: path.resolve(__dirname, "node_modules"),
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["env", "react"]
+                    }
+                }
+
+            }
+        ]
+    },
+    output: {
+        filename: '[name]_bundle.js',
+        path: BUID_DIR
+    }
+};
