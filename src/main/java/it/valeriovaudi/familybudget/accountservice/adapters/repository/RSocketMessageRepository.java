6492ac230cdc8cc0984661b0278b2aa90635cf03
mrFlick72 <valerio.vaudi@gmail.com>
2020-01-29 00:41:29 +0100

    concurrency issue fix

--- src/main/java/it/valeriovaudi/familybudget/accountservice/adapters/repository/RSocketMessageRepository.java
+++ src/main/java/it/valeriovaudi/familybudget/accountservice/adapters/repository/RSocketMessageRepository.java
@@ -7,7 +7,6 @@ import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.messaging.rsocket.RSocketRequester;
 import reactor.cache.CacheMono;
 import reactor.core.publisher.Mono;
-import reactor.core.publisher.Signal;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -38,11 +37,12 @@ public class RSocketMessageRepository implements MessageRepository {
                         }).map(stringStringHashMap -> stringStringHashMap));
 
 
-        CacheMono.lookup(key -> Mono.<Signal<Map<String, String>>>justOrEmpty((Signal) manager.getCache(CACHE_REGION).get(key, () -> null)), CACHE_KEY)
+        return CacheMono.lookup(
+                key -> Mono.fromRunnable(() -> manager.getCache(CACHE_REGION).get(key, () -> null)),
+                CACHE_KEY
+        )
                 .onCacheMissResume(supplier)
                 .andWriteWith((key, value) -> Mono.fromRunnable(() -> manager.getCache(CACHE_REGION).put(key, value)));
 
-
-        return (Publisher<Map<String, String>>) supplier.get();
     }
 }
