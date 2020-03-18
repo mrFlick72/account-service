82b37f37cbc9e52f908b8882a0237c0f90040886
mrFlick72 <valerio.vaudi@gmail.com>
2020-03-18 22:37:00 +0100

    kubernetes

--- src/main/java/it/valeriovaudi/familybudget/accountservice/AccountServiceApplication.java
+++ src/main/java/it/valeriovaudi/familybudget/accountservice/AccountServiceApplication.java
@@ -1,9 +1,13 @@
 package it.valeriovaudi.familybudget.accountservice;
 
+import it.valeriovaudi.vauthenticator.security.clientsecuritystarter.security.RedisOAuth2AuthorizedClientService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.integration.config.EnableIntegration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 
 @EnableCaching
 @EnableIntegration
@@ -12,5 +16,11 @@ public class AccountServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AccountServiceApplication.class, args);
+    }
+
+    @Bean
+    public RedisOAuth2AuthorizedClientService redisOAuth2AuthorizedClientService(RedisTemplate redisTemplate,
+                                                                                 ClientRegistrationRepository clientRegistrationRepository) {
+        return new RedisOAuth2AuthorizedClientService(redisTemplate, clientRegistrationRepository);
     }
 }
