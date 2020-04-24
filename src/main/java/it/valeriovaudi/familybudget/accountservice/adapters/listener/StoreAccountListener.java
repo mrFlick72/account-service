d1edb30180c53073008e6a2e28237dbc07d4f227
mrflick72 <valerio.vaudi@gmail.com>
2020-04-24 18:42:33 +0200

    account registration confirmation

--- src/main/java/it/valeriovaudi/familybudget/accountservice/adapters/listener/StoreAccountListener.java
+++ src/main/java/it/valeriovaudi/familybudget/accountservice/adapters/listener/StoreAccountListener.java
@@ -7,20 +7,24 @@ import it.valeriovaudi.familybudget.accountservice.domain.model.Phone;
 import it.valeriovaudi.familybudget.accountservice.domain.repository.AccountRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import reactor.core.publisher.Mono;
 
 import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
 
 @Slf4j
 public class StoreAccountListener {
     private final ObjectMapper objectMapper;
     private final AccountRepository accountRepository;
+    private final RabbitTemplate rabbitTemplate;
 
-    public StoreAccountListener(ObjectMapper objectMapper, AccountRepository accountRepository) {
+    public StoreAccountListener(ObjectMapper objectMapper, AccountRepository accountRepository, RabbitTemplate rabbitTemplate) {
         this.objectMapper = objectMapper;
         this.accountRepository = accountRepository;
+        this.rabbitTemplate = rabbitTemplate;
     }
 
     @RabbitListener(queues = "account-registration")
@@ -30,15 +34,25 @@ public class StoreAccountListener {
         try {
             HashMap<String, String> hashMap = objectMapper.readValue(accountRepresentation, HashMap.class);
             System.out.println(hashMap);
+            String email = hashMap.getOrDefault("email", "");
+            String emailBody = objectMapper.writeValueAsString(Map.of("email", email));
             Account account = new Account(
                     hashMap.getOrDefault("firstname", ""),
                     hashMap.getOrDefault("lastname", ""),
                     Date.dateFor("01/01/1970"),
-                    hashMap.getOrDefault("email", ""),
+                    email,
                     Phone.nullValue(),
                     Locale.ENGLISH
             );
-            Mono.from(accountRepository.save(account)).subscribe();
+            Mono<Object> rabbitSender =
+                    Mono.fromRunnable(
+                            () -> rabbitTemplate.convertAndSend("123", "vauthenticator-registration",emailBody)
+                    );
+
+            Mono.from(accountRepository.save(account))
+                    .flatMap(aVoid -> rabbitSender)
+                    .subscribe();
+
         } catch (Exception e) {
             log.error("user didn't found");
         }
