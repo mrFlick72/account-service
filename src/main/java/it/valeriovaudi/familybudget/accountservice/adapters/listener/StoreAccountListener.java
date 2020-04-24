73f9957a5605c75a16bdddcba9bfbb2ada36ab9c
mrflick72 <valerio.vaudi@gmail.com>
2020-04-24 18:58:54 +0200

    account registration confirmation

--- src/main/java/it/valeriovaudi/familybudget/accountservice/adapters/listener/StoreAccountListener.java
+++ src/main/java/it/valeriovaudi/familybudget/accountservice/adapters/listener/StoreAccountListener.java
@@ -1,5 +1,6 @@
 package it.valeriovaudi.familybudget.accountservice.adapters.listener;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import it.valeriovaudi.familybudget.accountservice.domain.model.Account;
 import it.valeriovaudi.familybudget.accountservice.domain.model.Date;
@@ -8,11 +9,11 @@ import it.valeriovaudi.familybudget.accountservice.domain.repository.AccountRepo
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.messaging.handler.annotation.SendTo;
 import reactor.core.publisher.Mono;
 
 import java.util.HashMap;
 import java.util.Locale;
-import java.util.Map;
 
 
 @Slf4j
@@ -28,14 +29,14 @@ public class StoreAccountListener {
     }
 
     @RabbitListener(queues = "account-registration")
-    public void getUserDetails(String accountRepresentation) {
+    @SendTo("vauthenticator-registration/account-stored")
+    public String getUserDetails(String accountRepresentation) {
         log.debug("mail: " + accountRepresentation);
 
         try {
             HashMap<String, String> hashMap = objectMapper.readValue(accountRepresentation, HashMap.class);
             System.out.println(hashMap);
             String email = hashMap.getOrDefault("email", "");
-            String emailBody = objectMapper.writeValueAsString(Map.of("email", email));
             Account account = new Account(
                     hashMap.getOrDefault("firstname", ""),
                     hashMap.getOrDefault("lastname", ""),
@@ -44,17 +45,11 @@ public class StoreAccountListener {
                     Phone.nullValue(),
                     Locale.ENGLISH
             );
-            Mono<Object> rabbitSender =
-                    Mono.fromRunnable(
-                            () -> rabbitTemplate.convertAndSend("123", "vauthenticator-registration",emailBody)
-                    );
-
-            Mono.from(accountRepository.save(account))
-                    .flatMap(aVoid -> rabbitSender)
-                    .subscribe();
-
-        } catch (Exception e) {
+            Mono.from(accountRepository.save(account)).subscribe();
+            return accountRepresentation;
+        } catch (RuntimeException | JsonProcessingException e) {
             log.error("user didn't found");
+            throw new RuntimeException();
         }
     }
 
