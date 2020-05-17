92159515cddc104bb2eb222418c12cdff82fd6ee
mrflick72 <valerio.vaudi@gmail.com>
2020-05-17 13:01:04 +0200

    context path issue on k8s profile

--- src/main/java/it/valeriovaudi/familybudget/accountservice/AccountServiceApplication.java
+++ src/main/java/it/valeriovaudi/familybudget/accountservice/AccountServiceApplication.java
@@ -2,6 +2,8 @@ package it.valeriovaudi.familybudget.accountservice;
 
 import io.micrometer.core.instrument.MeterRegistry;
 import it.valeriovaudi.familybudget.accountservice.web.endpoint.ContextPathProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
@@ -9,7 +11,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Profile;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.integration.config.EnableIntegration;
 import org.springframework.web.server.WebFilter;
@@ -21,31 +22,35 @@ import java.util.Optional;
 @SpringBootApplication
 public class AccountServiceApplication {
 
+    private final static Logger LOGGER = LoggerFactory.getLogger(AccountServiceApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(AccountServiceApplication.class, args);
     }
 
     @Bean
-    public ContextPathProvider contextPathProvider(ServerProperties  serverProperties){
-        String contextPath = Optional.ofNullable(serverProperties.getServlet().getContextPath()).orElse("/");
+    public ContextPathProvider contextPathProvider(ServerProperties serverProperties) {
+        String contextPath = Optional.ofNullable(serverProperties.getServlet())
+                .flatMap(servlet -> Optional.ofNullable(servlet.getContextPath()))
+                .orElse("/");
         return new ContextPathProvider(contextPath);
     }
 
     @Bean
-    @Profile("!kubernetes")
     public WebFilter contextPathWebFilter(ServerProperties serverProperties) {
         String contextPath = serverProperties.getServlet().getContextPath();
-        System.out.println(contextPath);
-        return (exchange, chain) -> {
-            ServerHttpRequest request = exchange.getRequest();
-            if (request.getURI().getPath().startsWith(contextPath)) {
-                return chain.filter(
-                        exchange.mutate()
-                                .request(request.mutate().contextPath(contextPath).build())
-                                .build());
-            }
-            return chain.filter(exchange);
-        };
+        LOGGER.info("contextPath: " + contextPath);
+        return contextPath != null ?
+                (exchange, chain) -> {
+                    ServerHttpRequest request = exchange.getRequest();
+                    if (request.getURI().getPath().startsWith(contextPath)) {
+                        return chain.filter(
+                                exchange.mutate()
+                                        .request(request.mutate().contextPath(contextPath).build())
+                                        .build());
+                    }
+                    return chain.filter(exchange);
+                } : (exchange, chain) -> chain.filter(exchange);
     }
 
     @Bean
