e5a6033ac22f6a0dc2430cc39870162f615d1862
mrFlick72 <valerio.vaudi@gmail.com>
2020-12-29 15:46:07 +0100

    AccountEndPointTest

--- src/test/java/it/valeriovaudi/familybudget/accountservice/web/endpoint/AccountEndPointTest.java
+++ src/test/java/it/valeriovaudi/familybudget/accountservice/web/endpoint/AccountEndPointTest.java
@@ -8,27 +8,21 @@ import it.valeriovaudi.familybudget.accountservice.domain.repository.AccountRepo
 import it.valeriovaudi.familybudget.accountservice.web.adapter.AccountAdapter;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.BodyInserters;
 import reactor.core.publisher.Mono;
 
 import java.util.Locale;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.mock;
+import static org.springframework.security.test.web.reactive.server.SecurityMockServerConfigurers.*;
 
 @WebFluxTest(value = {AccountEndPoint.class, AdapterTestConfig.class})
 class AccountEndPointTest {
@@ -46,22 +40,18 @@ class AccountEndPointTest {
 
     @BeforeEach
     public void setUp() {
-        webTestClient = WebTestClient.bindToApplicationContext(context).build();
+        webTestClient = WebTestClient.bindToApplicationContext(context)
+                .apply(springSecurity())
+                .apply(mockUser())
+                .build();
     }
 
-    /*
-     *      Mono.from(accountRepository.findByMail(serverRequest.pathVariable("mail")))
-     *                            .map(accountAdapter::domainToRepresentationModel)
-     *                            .flatMap(accountRepresentation ->
-     *                                   ServerResponse.ok().body(BodyInserters.fromValue(accountRepresentation)))
-     * */
     @Test
-    @WithMockUser
     void getUserInfoFromEmail() {
         Account account = new Account("FIRST_NAME",
                 "LAST_NAME",
                 Date.dateFor("01/01/1970"),
-                "user.mail@mail",
+                "user.mail@mail.com",
                 Phone.nullValue(),
                 Locale.ITALIAN
         );
@@ -70,21 +60,44 @@ class AccountEndPointTest {
                 .willReturn(Mono.just(account));
 
 
-        String expected = "{\"firstName\":\"FIRST_NAME\",\"lastName\":\"LAST_NAME\",\"birthDate\":\"01/01/1970\",\"mail\":\"user.mail@mail\",\"phone\":\"\"}";
+        String expected = "{\"firstName\":\"FIRST_NAME\",\"lastName\":\"LAST_NAME\",\"birthDate\":\"01/01/1970\",\"mail\":\"user.mail@mail.com\",\"phone\":\"\"}";
         webTestClient.get()
-                        .uri("/user.mail@mail.com/mail")
-                        .accept(MediaType.APPLICATION_JSON)
-                        .exchange()
-                        .expectStatus().is2xxSuccessful()
-                        .expectBody().json(expected);
+                .uri("/user.mail@mail.com/mail")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().is2xxSuccessful()
+                .expectBody().json(expected);
+    }
+
+    @Test
+    void updateAccount() {
+        Account account = new Account("FIRST_NAME",
+                "LAST_NAME",
+                Date.dateFor("01/01/1970"),
+                "user.mail@mail.com",
+                Phone.nullValue(),
+                Locale.ENGLISH
+        );
+
+        System.out.println(account);
+        given(updateAccount.execute(account))
+                .willReturn(Mono.fromRunnable(() -> {
+                }));
 
 
+        String content = "{\"firstName\":\"FIRST_NAME\",\"lastName\":\"LAST_NAME\",\"birthDate\":\"01/01/1970\",\"mail\":\"user.mail@mail.com\",\"phone\":\"\"}";
+        webTestClient.mutateWith(csrf()).put()
+                .uri("/user.mail@mail.com/mail")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromValue(content))
+                .exchange()
+                .expectStatus().is2xxSuccessful();
     }
 }
 
 @Configuration
 class AdapterTestConfig {
-
 
     @Bean
     public AccountAdapter accountAdapter() {
