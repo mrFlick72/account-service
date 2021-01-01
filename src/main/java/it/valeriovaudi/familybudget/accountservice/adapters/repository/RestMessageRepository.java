800f196e0ec3a861bde014fbb58b72f042f01963
mrFlick72 <valerio.vaudi@gmail.com>
2021-01-01 22:50:44 +0100

    test fixed

--- src/main/java/it/valeriovaudi/familybudget/accountservice/adapters/repository/RestMessageRepository.java
+++ src/main/java/it/valeriovaudi/familybudget/accountservice/adapters/repository/RestMessageRepository.java
@@ -3,6 +3,7 @@ package it.valeriovaudi.familybudget.accountservice.adapters.repository;
 import it.valeriovaudi.familybudget.accountservice.adapters.cache.ReactiveCacheManager;
 import it.valeriovaudi.familybudget.accountservice.domain.repository.MessageRepository;
 import org.reactivestreams.Publisher;
+import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
@@ -28,10 +29,8 @@ public class RestMessageRepository implements MessageRepository {
 
     @Override
     public Publisher<Map<String, String>> messages() {
-        System.out.println("applicationId: " + applicationId);
-        System.out.println("/messages/" + applicationId);
-        return cacheManager.getFromCache().log()
-                .switchIfEmpty(loader()).log()
+        return cacheManager.getFromCache()
+                .switchIfEmpty(loader())
                 .flatMap(o -> cacheManager.updateCache((Map<String, String>) o));
     }
 
--- src/test/java/it/valeriovaudi/familybudget/accountservice/adapters/repository/RestMessageRepositoryTest.java
+++ src/test/java/it/valeriovaudi/familybudget/accountservice/adapters/repository/RestMessageRepositoryTest.java
@@ -1,25 +1,27 @@
 package it.valeriovaudi.familybudget.accountservice.adapters.repository;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.common.Json;
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
 import com.github.tomakehurst.wiremock.stubbing.StubMapping;
 import it.valeriovaudi.familybudget.accountservice.adapters.cache.ReactiveCacheManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
-import wiremock.com.fasterxml.jackson.databind.JsonNode;
 
 import java.time.Duration;
+import java.util.HashMap;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
-import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 import static it.valeriovaudi.familybudget.accountservice.TestingFixture.i18nsMessage;
 import static it.valeriovaudi.familybudget.accountservice.TestingFixture.newReactiveRedisTemplate;
@@ -30,11 +32,12 @@ class RestMessageRepositoryTest {
     private ReactiveCacheManager cacheManager;
 
     private ObjectMapper objectMapper = new ObjectMapper();
-    private WireMockServer wireMockServer = new WireMockServer(wireMockConfig().dynamicPort());
+    private WireMockServer wireMockServer = new WireMockServer(WireMockConfiguration.options().dynamicPort());
 
     @BeforeEach
     void setUp() {
         wireMockServer.start();
+        configureFor("localhost", wireMockServer.port());
 
         cacheManager = new ReactiveCacheManager(Duration.ofSeconds(1), newReactiveRedisTemplate());
         restMessageRepository = new RestMessageRepository(
@@ -50,19 +53,23 @@ class RestMessageRepositoryTest {
         wireMockServer.stop();
     }
 
-//    @Test()
+    @Test()
     void whenCacheIsEmpty() throws JsonProcessingException {
+        String body = objectMapper.writeValueAsString(i18nsMessage());
+
         StubMapping stubMapping = stubFor(
                 get(urlPathEqualTo("/messages/account-service"))
                         .willReturn(aResponse()
                                 .withHeader("Content-Type", "application/json")
-                                .withBody("{}")));
+                                .withBody(body)));
 
         wireMockServer.addStubMapping(stubMapping);
 
         StepVerifier.create(restMessageRepository.messages())
                 .expectNext(i18nsMessage())
                 .verifyComplete();
+
+        wireMockServer.verify(getRequestedFor(urlEqualTo("/messages/account-service")));
     }
 
     @Test
