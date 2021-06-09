05f64374918de2ccb687f6ffe34567ce53200dc8
mrflick72 <Back72Star>
2021-06-09 23:04:46 +0200

    send message update event

--- src/internal/messaging/event_sender.go
+++ src/internal/messaging/event_sender.go
@@ -1,8 +1,28 @@
 package messaging
 
+import (
+	"encoding/json"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/sqs"
+)
+
 type EventSender interface {
 	SendEvent(event interface{}) error
 }
 
 type SqsEventSender struct {
+	client   *sqs.SQS
+	queueURL string
+}
+
+func (receiver *SqsEventSender) SendEvent(event interface{}) error {
+	body, err := json.Marshal(event)
+	if err == nil {
+		input := sqs.SendMessageInput{
+			MessageBody: aws.String(string(body)),
+			QueueUrl:    &receiver.queueURL,
+		}
+		_, err = receiver.client.SendMessage(&input)
+	}
+	return err
 }
--- src/test/terraform/sqs.tf
+++ src/test/terraform/sqs.tf
@@ -2,3 +2,7 @@ resource "aws_sqs_queue" "account_sync_queue_staging" {
   name = "TESTING_Account_Service_Sync_queue"
   message_retention_seconds = 300
 }
+
+output "sqs_queue" {
+  value = aws_sqs_queue.account_sync_queue_staging
+}
