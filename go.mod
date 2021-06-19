28e71a3b47cbecfdd8254479a0581e900c54e2a3
mrflick72 <valerio.vaudi@gmail.com>
2021-06-19 15:33:08 +0200

    jwk component to retrieve rsa key from oauth2 server

--- go.mod
+++ go.mod
@@ -5,15 +5,12 @@ go 1.16
 require (
 	github.com/ajg/form v1.5.1 // indirect
 	github.com/aws/aws-sdk-go v1.38.56
-	github.com/fasthttp-contrib/websocket v0.0.0-20160511215533-1f3b11f56072 // indirect
+	github.com/go-resty/resty/v2 v2.6.0
 	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/imkira/go-interpol v1.1.0 // indirect
-	github.com/k0kubun/colorstring v0.0.0-20150214042306-9440f1994b88 // indirect
 	github.com/kataras/iris/v12 v12.1.8
-	github.com/mattn/go-colorable v0.1.8 // indirect
+	github.com/lestrrat-go/jwx v1.2.1
 	github.com/moul/http2curl v1.0.0 // indirect
-	github.com/onsi/ginkgo v1.16.4 // indirect
-	github.com/onsi/gomega v1.13.0 // indirect
 	github.com/sergi/go-diff v1.2.0 // indirect
 	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
 	github.com/smartystreets/goconvey v1.6.4 // indirect
@@ -23,5 +20,5 @@ require (
 	github.com/yalp/jsonpath v0.0.0-20180802001716-5cc68e5049a0 // indirect
 	github.com/yudai/gojsondiff v1.0.0 // indirect
 	github.com/yudai/golcs v0.0.0-20170316035057-ecda9a501e82 // indirect
-	github.com/yudai/pp v2.0.1+incompatible // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,13 +1,60 @@
 package main
 
 import (
+	"context"
+	"crypto/rsa"
+	"fmt"
+	"github.com/lestrrat-go/jwx/jwk"
 	"github.com/mrflick72/account-service/src/config/application"
+	"log"
 	"sync"
 )
 
 func main() {
+	main2()
 	wg := &sync.WaitGroup{}
 	wg.Add(1)
 	go application.NewApplicationServer(wg)
 	wg.Wait()
+}
+
+func main2() {
+	// Example jwk from https://www.googleapis.com/oauth2/v3/certs (but with only one cert for simplicity)
+	jwkJSON := `{
+  "keys": [ 
+    {
+      "kty": "RSA",
+      "n": "o76AudS2rsCvlz_3D47sFkpuz3NJxgLbXr1cHdmbo9xOMttPMJI97f0rHiSl9stltMi87KIOEEVQWUgMLaWQNaIZThgI1seWDAGRw59AO5sctgM1wPVZYt40fj2Qw4KT7m4RLMsZV1M5NYyXSd1lAAywM4FT25N0RLhkm3u8Hehw2Szj_2lm-rmcbDXzvjeXkodOUszFiOqzqBIS0Bv3c2zj2sytnozaG7aXa14OiUMSwJb4gmBC7I0BjPv5T85CH88VOcFDV51sO9zPJaBQnNBRUWNLh1vQUbkmspIANTzj2sN62cTSoxRhSdnjZQ9E_jraKYEW5oizE9Dtow4EvQ",
+      "use": "sig",
+      "alg": "RS256",
+      "e": "AQAB",
+      "kid": "6a8ba5652a7044121d4fedac8f14d14c54e4895b"
+    }
+  ]
+}
+`
+
+	set, err := jwk.Parse([]byte(jwkJSON))
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(set)
+	for it := set.Iterate(context.Background()); it.Next(context.Background()); {
+		pair := it.Pair()
+		key := pair.Value.(jwk.Key)
+
+		var rawkey interface{} // This is the raw key, like *rsa.PrivateKey or *ecdsa.PrivateKey
+		if err := key.Raw(&rawkey); err != nil {
+			log.Printf("failed to create public key: %s", err)
+			return
+		}
+
+		// We know this is an RSA Key so...
+		rsa, ok := rawkey.(*rsa.PublicKey)
+		if !ok {
+			panic(fmt.Sprintf("expected ras key, got %T", rawkey))
+		}
+		// As this is a demo just dump the key to the console
+		fmt.Println(rsa)
+	}
 }
