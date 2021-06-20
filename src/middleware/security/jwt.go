c31a7a99fac1e5e0ac958ba969456840cf73371e
mrflick72 <valerio.vaudi@gmail.com>
2021-06-20 23:15:21 +0200

    jwt middleware

--- src/middleware/security/jwt.go
+++ src/middleware/security/jwt.go
@@ -1,30 +1,53 @@
 package security
 
 import (
+	"context"
 	"github.com/kataras/iris/v12"
 	"github.com/lestrrat-go/jwx/jwk"
 	"github.com/lestrrat-go/jwx/jwt"
 	"time"
 )
 
-func SetUpOAuth2(app *iris.Application, jwk Jwk) {
+func SetUpOAuth2(app *iris.Application, jwk Jwk, uriAuthMapper map[string]string) {
 	sets, _ := jwk.JwkSets()
-	var middleware = NewOAuth2Middleware(sets)
+	var middleware = NewOAuth2Middleware(sets, uriAuthMapper)
 	app.Use(middleware)
 }
 
-func NewOAuth2Middleware(keySet jwk.Set) func(ctx iris.Context) {
+func NewOAuth2Middleware(keySet jwk.Set, mapper map[string]string) func(ctx iris.Context) {
 	return func(ctx iris.Context) {
 		authorization := authorizationHeaderFor(ctx)
 
 		jwt, _ := jwt.ParseString(authorization, jwt.WithKeySet(keySet))
-		//ctx.Request().Context().Value("")
 		if time.Now().After(jwt.Expiration()) {
 			ctx.StatusCode(401)
 			return
 		}
+		userName, _ := jwt.PrivateClaims()["email"].(string)
+		authorities, _ := jwt.PrivateClaims()["authorities"].([]string)
 
+		allowedAuthority := mapper[ctx.Path()]
+		if ok := contains(authorities, allowedAuthority); !ok {
+			ctx.StatusCode(403)
+			return
+		}
+
+		newContext := context.WithValue(ctx.Request().Context(), "user", OAuth2User{
+			UserName:    userName,
+			Authorities: authorities,
+		})
+		ctx.Request().WithContext(newContext)
 	}
+}
+
+func contains(slice []string, item string) bool {
+	set := make(map[string]struct{}, len(slice))
+	for _, s := range slice {
+		set[s] = struct{}{}
+	}
+
+	_, ok := set[item]
+	return ok
 }
 
 func authorizationHeaderFor(ctx iris.Context) string {
@@ -35,8 +58,5 @@ func authorizationHeaderFor(ctx iris.Context) string {
 
 type OAuth2User struct {
 	UserName    string
-	AccessToken string
-	IdToken     string
-	Expire      time.Time
 	Authorities []string
 }
