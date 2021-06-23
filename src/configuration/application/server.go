b85772ec48e0c0c9aa12082736e4b9001d5f55b5
mrflick72 <valerio.vaudi@gmail.com>
2021-06-23 23:24:28 +0200

    configuration

--- src/configuration/application/server.go
+++ src/configuration/application/server.go
@@ -1,16 +1,18 @@
 package application
 
 import (
-	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/kataras/iris/v12"
 	"github.com/kataras/iris/v12/middleware/logger"
 	"github.com/kataras/iris/v12/middleware/recover"
 	"github.com/mrflick72/account-service/src/api"
+	"github.com/mrflick72/account-service/src/configuration"
 	"github.com/mrflick72/account-service/src/model/repository"
 	"sync"
 )
+
+var manager = configuration.GetConfigurationManagerInstance()
 
 func newWebServer() *iris.Application {
 	app := iris.New()
@@ -23,9 +25,7 @@ func NewApplicationServer(wg *sync.WaitGroup) {
 	app := newWebServer()
 
 	ConfigureAccountEndpoints(ConfigureAccountRepository(), app)
-
-	// Listen and serve on 0.0.0.0:8080
-	app.Listen("0.0.0.0:9000")
+	app.Listen(manager.GetConfigFor("server.port"))
 	wg.Done()
 }
 
@@ -33,11 +33,11 @@ func ConfigureAccountRepository() repository.AccountRepository {
 	sess := session.Must(session.NewSessionWithOptions(session.Options{
 		SharedConfigState: session.SharedConfigEnable,
 	}))
-	svc := dynamodb.New(sess, &aws.Config{Region: aws.String("us-east-1"), Endpoint: aws.String("http://localhost:8000")})
+	svc := dynamodb.New(sess)
 
 	return &repository.DynamoAccountRepository{
 		Client:    svc,
-		TableName: "TESTING_Account_Service",
+		TableName: manager.GetConfigFor("datasource.table-name"),
 	}
 }
 
