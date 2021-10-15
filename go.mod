4563d1ca118f0750e4d2117d4296bad6c502da5a
mrflick72 <valerio.vaudi@gmail.com>
2021-10-16 01:24:06 +0200

    event sender coming from framework

--- go.mod
+++ go.mod
@@ -5,6 +5,6 @@ go 1.16
 require (
 	github.com/aws/aws-sdk-go v1.38.56
 	github.com/kataras/iris/v12 v12.1.8
-	github.com/mrflick72/cloud-native-golang-framework v0.1.4
+	github.com/mrflick72/cloud-native-golang-framework v0.1.5
 	github.com/stretchr/testify v1.7.0
 )
--- src/config/config.go
+++ src/config/config.go
@@ -6,11 +6,11 @@ import (
 	"github.com/aws/aws-sdk-go/service/sqs"
 	"github.com/kataras/iris/v12"
 	"github.com/mrflick72/account-service/src/api"
-	"github.com/mrflick72/account-service/src/internal/messaging"
 	"github.com/mrflick72/account-service/src/model/repository"
 	"github.com/mrflick72/account-service/src/model/usecase"
 	"github.com/mrflick72/cloud-native-golang-framework"
 	"github.com/mrflick72/cloud-native-golang-framework/configuration"
+	awssqs "github.com/mrflick72/cloud-native-golang-framework/messaging/aws/sqs"
 	"sync"
 )
 
@@ -34,7 +34,7 @@ func ConfigureAccountUpdater(repository repository.AccountRepository) *usecase.U
 	}))
 	svc := sqs.New(sess)
 
-	sender := messaging.SqsEventSender{
+	sender := awssqs.SqsEventSender{
 		Client:   svc,
 		QueueURL: manager.GetConfigFor("account.event.updates.sqs-queue-url"),
 	}
